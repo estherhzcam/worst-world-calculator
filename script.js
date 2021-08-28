0005d2f2897c413de48107d9e8dfe30d7e036c34
Esther <esth2577@stud.kea.dk>
2021-08-28 22:20:00 +0200

    clear option added and mistakes in if decimal corrected

--- script.js
+++ script.js
@@ -9,6 +9,8 @@ let checkbox = document.querySelector("#doround");
 let result;
 let resultrounded;
 
+document.querySelector("#clear").addEventListener("click", clearResults)
+
 function settingUp() {
 
     document.querySelector("#calculate").addEventListener("click", readFirstNumber)
@@ -66,27 +68,27 @@ if (checkbox.checked == true) {
         resultrounded = result.toFixed(1)
         console.log(resultrounded)
     }
-    else if (decimals == 1) {
+    else if (decimals == 2) {
         resultrounded = result.toFixed(2)
         console.log(resultrounded)
     }
-    else if (decimals == 1) {
+    else if (decimals == 3) {
         resultrounded = result.toFixed(3)
         console.log(resultrounded)
     }
-    else if (decimals == 1) {
+    else if (decimals == 4) {
         resultrounded = result.toFixed(4)
         console.log(resultrounded)
     }
-    else if (decimals == 1) {
+    else if (decimals == 5) {
         resultrounded = result.toFixed(5)
         console.log(resultrounded)
     }
-    else if (decimals == 1) {
+    else if (decimals == 6) {
         resultrounded = result.toFixed(6)
         console.log(resultrounded)
     }
-    else if (decimals == 1) {
+    else if (decimals == 7) {
         resultrounded = result.toFixed(7)
         console.log(resultrounded)
     }
@@ -94,6 +96,13 @@ if (checkbox.checked == true) {
         resultrounded = result.toFixed(8)
         console.log(resultrounded)
     }
+    let viewresult = document.createElement("li")
+document.querySelector("#results").appendChild(viewresult);
+viewresult.append(resultrounded)
+/*populate first result field*/
+document.querySelector("input[id=firstnumber]").value = resultrounded;
+/* clean imput fields */
+document.querySelector("input[id=secondnumber]").value = "";
 }
 /* inserting result at the bottom of the list https://developer.mozilla.org/en-US/docs/Web/API/Element/after */
 
@@ -102,7 +111,7 @@ else  {
     console.log("rounding no")
 let viewresult = document.createElement("li")
 document.querySelector("#results").appendChild(viewresult);
-viewresult.after(result)
+viewresult.append(result)
 /*populate first result field*/
 document.querySelector("input[id=firstnumber]").value = result;
 /* clean imput fields */
@@ -110,3 +119,9 @@ document.querySelector("input[id=secondnumber]").value = "";
 
 }
 }
+
+function clearResults () {
+    console.log("clearing")
+    document.querySelector("input[id=firstnumber]").value = "";
+    document.querySelector("input[id=secondnumber]").value = "";
+    document.querySelector("#results").innerHTML = '';}
